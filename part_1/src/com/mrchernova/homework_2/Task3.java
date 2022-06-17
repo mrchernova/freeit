688b7d93fd8186d1e1fbcd1016057645efa233b2
cherraniram@gmail.com <cherraniram@gmail.com>
2022-06-17 23:26:44 +0300

    homework 2: task3. Splitting seconds into days(hours,weeks...)

--- part_1/src/com/mrchernova/homework_2/Task3.java
+++ part_1/src/com/mrchernova/homework_2/Task3.java
@@ -6,20 +6,28 @@ package com.mrchernova.homework_2;
  * В) часов + минут + секунд,
  * С) дней + часов + минут + секунд,
  * D) недель + дней + часов + минут + секунд.
- * по аналогии с примером выше.
  */
 
 public class Task3 {
     public static void main(String[] args) {
-        int a = 4500;
-/*
-        int sec = a % 60;
-        System.out.println(sec);
-        int m = (a - sec) / 60;
+        int s = 4500;
 
+        int sec = s % 60;
+
+        int m = (s - sec) / 60;
         int min = m % 60;
-        int h = (m - min) / 60;
 
-        System.out.println(h + " h  " + min + " min  " + sec + " sec");
-  */  }
+        int h = (m - min) / 60;
+        int hours = h % 24;
+
+        int d = (h - hours) / 24;
+        int days = d % 7;
+
+        int w = (d - days) / 7;
+
+        System.out.println(                                                           m + " min  " + sec + " sec");
+        System.out.println(                                        h + " hours  " + min + " min  " + sec + " sec");
+        System.out.println(                    d + " days  " + hours + " hours  " + min + " min  " + sec + " sec");
+        System.out.println(w + " weeks  " + days + " days  " + hours + " hours  " + min + " min  " + sec + " sec");
+    }
 }
