45841b04faf96625d77918e3891327f7c29be285
cherraniram@gmail.com <cherraniram@gmail.com>
2022-06-18 11:53:18 +0300

    homework 2: task3. Reverse number

--- part_1/src/com/mrchernova/homework_2/Task4.java
+++ part_1/src/com/mrchernova/homework_2/Task4.java
@@ -6,5 +6,21 @@ package com.mrchernova.homework_2;
  */
 
 public class Task4 {
+    public static void main(String[] args) {
+        int n = 1030;
+        int x;
+        int a = 0;
 
+        if (n >= 1000 && n <= 9999) {
+            System.out.println("Вы ввыли число " + n);
+            for (int i = 0; i < 4; i++) {
+                x = n % 10;
+                a = a * 10 + x;
+                n = n / 10;
+            }
+            System.out.println("Ваше число в обратном порядке = " + a);
+        } else {
+            System.out.println("Введенное число не 4-х значное или не натуральное");
+        }
+    }
 }
