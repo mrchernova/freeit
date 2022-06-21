9d464111439e6a797b075c26cb407b0f360f6b99
cherraniram@gmail.com <cherraniram@gmail.com>
2022-06-21 22:51:45 +0300

    homework 3: ExtraTask5. Rectangle hole and cardboard circle

--- part_1/src/com/chernova/homework_3/ExtraTask5.java
+++ part_1/src/com/chernova/homework_3/ExtraTask5.java
@@ -8,5 +8,15 @@ package com.chernova.homework_3;
 public class ExtraTask5 {
     public static void main(String[] args) {
 
+        float a = 4.3f;
+        float b = 3.1f;
+        float r = 2.3f;
+
+        if (Math.pow(r, 2) >= Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))) {
+            System.out.println("possible to cover");
+        } else {
+            System.out.println("impossible to cover");
+        }
+
     }
 }
