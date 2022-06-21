c41d2a43fb6bb639d50e67cbf2419a46fdec2566
cherraniram@gmail.com <cherraniram@gmail.com>
2022-06-21 21:00:45 +0300

    homework 3: Task2. 5 ways to create/get String

--- part_1/src/com/chernova/homework_3/Task2.java
+++ part_1/src/com/chernova/homework_3/Task2.java
@@ -6,6 +6,17 @@ package com.chernova.homework_3;
 
 public class Task2 {
     public static void main(String[] args) {
+        String s1 = "hi";
+        String s2 = new String("hey");
+        String s3 = s1;
+        String s4 = Integer.toString(42);
+        String s5 = String.valueOf('h');  ;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
 
     }
 }
